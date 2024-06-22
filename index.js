63c7a5771b6ee74d21735a03d39df43e382861b6
Misbah Jameel <162004635+Misbah-jameel@users.noreply.github.com>
2024-06-22 22:02:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// Switch cases
+var grade = "a";
+switch (grade) {
+    case "A":
+        console.log("You are bright student");
+        break;
+    case "B":
+        console.log("second");
+        break;
+    case "C":
+        console.log("Third");
+        break;
+    case "D":
+        console.log("fail");
+        break;
+    default:
+        console.log("invalid output");
+}
---
+++ index.ts
@@ -0,0 +1,19 @@
+// Switch cases
+let grade:string = "A";
+     switch(grade) {
+    case "A":
+    console.log("You are bright student")
+       break;
+           case "B":
+         console.log("second");
+            break;
+                  case "C":
+                console.log("Third");
+                  break;
+                          case "D":
+                       console.log("fail");
+                       break;
+                          default:
+        console.log("invalid output");
+        
+};
